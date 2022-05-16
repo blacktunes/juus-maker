84e47c4654c6bebf9c016b8de0f22938fd93c879
blacktunes <blacktunes@foxmail.com>
2022-05-16 19:34:18 +0800

    build: 修改部分配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,5 +13,10 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  },
+  globals: {
+    defineEmits: 'readonly',
+    defineProps: 'readonly',
+    defineExpose: 'readonly'
   }
 }
