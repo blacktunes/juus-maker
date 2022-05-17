8d3ddc9ac2fa8005fe5ec5bec68ee29fa3f13e5f
blacktunes <blacktunes@foxmail.com>
2022-05-18 02:00:04 +0800

    feat: 增加头像数据

--- src/assets/scripts/ship.js
+++ src/assets/scripts/ship.js
@@ -9,14 +9,30 @@ let data = {
   }
 }
 
+let avatar = {}
+
 dataList.forEach(name => {
-  data = { ...data, ...require(`@/assets/data/${name}`) }
+  if (name === 'avatar.json') {
+    avatar = require(`@/assets/data/${name}`)
+  } else {
+    data = { ...data, ...require(`@/assets/data/${name}`) }
+  }
 })
 
-const getData = (name) => {
-  return data[name]
-    ? data[name]
-    : { avatar: '', name: '' }
+for (const key in data) {
+  if (avatar[key]) data[key].avatar = avatar[key]
+}
+
+const getData = (key, text) => {
+  return {
+    key,
+    text,
+    ...(
+      data[key]
+        ? { avatar: data[key].avatar, name: data[key].name }
+        : { avatar: '', name: '' }
+    )
+  }
 }
 
 export { getData }
