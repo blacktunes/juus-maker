d6949bc21272b4168a572be7cc24999f024aa0cf
blacktunes <blacktunes@foxmail.com>
2022-11-29 20:30:07 +0800

    feat: 优化数据更新脚本

--- src/assets/data/index.js
+++ src/assets/data/index.js
@@ -1,41 +1,8 @@
 import { reactive } from 'vue'
 
-const dataList = require.context('@/assets/data', false, /.txt$/).keys().map(item => {
-  return item.substring(2)
-})
-
-const loadTxt = text => {
-  const list = {}
-  let group = ''
-  let type = ''
-
-  text.split('\n').forEach(item => {
-    if (item.startsWith('===')) {
-      type = item.replaceAll('=', '').replace('\r', '')
-      return
-    }
-    if (item.startsWith('==')) {
-      group = item.replaceAll('=', '').replace('阵营', '').replace('\r', '')
-      return
-    }
-    if (item.includes('@')) {
-      const temp = item.replace(/\{|\}|小图标\||<br>/g, '').replace('\r', '').split('@')
-      const key = temp[0].split('|')[0]
-      list[key] = {
-        avatar: '',
-        name: temp[1],
-        type,
-        group
-      }
-    }
-  })
-
-  return list
-}
-
 const avatar = require('@/assets/data/avatar.json')
 
-let data = reactive({
+const data = reactive({
   指挥官: {
     avatar: require('@/assets/images/commander.jpg'),
     name: '指挥官'
@@ -43,11 +10,8 @@ let data = reactive({
   自定义: {
     avatar: '',
     name: '自定义'
-  }
-})
-
-dataList.forEach(name => {
-  data = { ...data, ...loadTxt(require(`@/assets/data/${name}`).txt) }
+  },
+  ...require('@/assets/data/ship.json')
 })
 
 for (const key in data) {
