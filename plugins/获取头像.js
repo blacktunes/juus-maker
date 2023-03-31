5cd6b60f0772a1f69ecf7526de5ce30c4bfa2c75
blacktunes <blacktunes@foxmail.com>
2023-04-01 01:55:00 +0800

    refactor: 修改脚本目录

--- plugins/获取头像.js
+++
@@ -1,5 +0,0 @@
-const avatar = {}
-document.getElementsByClassName('xtb-image').forEach(item => {
-  avatar[item.children[0].attributes[1].nodeValue] = decodeURI(item.children[0].children[0].currentSrc).replace('30px', '85px')
-})
-console.table(avatar)
