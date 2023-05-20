79d279358cac9244fb412233a006225f98f90276
blacktunes <blacktunes@foxmail.com>
2023-05-20 17:08:32 +0800

    feat: 增加访问统计

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
+import { inject } from '@vercel/analytics'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './style.styl'
 
-createApp(App).use(router).mount('#app')
+createApp(App)
+  .use(router)
+  .mount('#app')
+
+if (process.env.NODE_ENV !== 'development') {
+  inject()
+}
