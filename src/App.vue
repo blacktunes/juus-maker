7f93677f20411c5cfc3834647e2b07058b15e68f
blacktunes <blacktunes@foxmail.com>
2023-08-13 17:41:38 +0800

    perf: 优化Loading组件

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,63 @@
 <template>
+  <transition name="fade">
+    <div
+      v-if="setting.screenshot"
+      class="loading"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="50"
+        height="50"
+        viewBox="0 0 400 400"
+      >
+        <circle
+          class="circle"
+          cx="200"
+          cy="200"
+          r="180"
+          stroke="#ddd"
+          stroke-width="40"
+          stroke-linecap="round"
+          stroke-dasharray="1194"
+          fill="none"
+        />
+      </svg>
+    </div>
+  </transition>
   <RouterView />
 </template>
 
 <script lang="ts" setup>
 import { getDB } from './store/custom'
+import { setting } from '@/store/setting'
 
 getDB()
 </script>
+<style lang="stylus" scoped>
+.loading
+  z-index 999
+  position absolute
+  width 100%
+  height 100%
+  background rgba(0, 0, 0, 0.5)
+  display flex
+  justify-content center
+  align-items center
+
+  .circle
+    stroke-dashoffset 0
+    animation loading 2s infinite
+    transform-origin center
+
+@keyframes loading
+  0%
+    stroke-dashoffset 0
+    transform rotate(0deg)
+
+  50%
+    stroke-dashoffset 1194
+
+  100%
+    stroke-dashoffset 0
+    transform rotate(360deg)
+</style>
--- src/views/JUUs/Index.vue
+++ src/views/JUUs/Index.vue
@@ -13,31 +13,6 @@
         }"
       >
         <transition name="fade">
-          <div
-            v-if="setting.screenshot"
-            class="loading"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="50"
-              height="50"
-              viewBox="0 0 400 400"
-            >
-              <circle
-                class="circle"
-                cx="200"
-                cy="200"
-                r="180"
-                stroke="#ddd"
-                stroke-width="40"
-                stroke-linecap="round"
-                stroke-dasharray="1194"
-                fill="none"
-              />
-            </svg>
-          </div>
-        </transition>
-        <transition name="fade">
           <ShipSelect
             v-show="select.show"
             class="select"
@@ -100,7 +75,6 @@ import { ref, computed, onMounted } from 'vue'
 import JUUsSelect from './JUUsSelect.vue'
 import JUUs from './JUUs.vue'
 import data, { getDB } from '@/store/juus'
-import { setting } from '@/store/setting'
 import ShipSelect from '@/components/Ship/ShipSelect.vue'
 import { select } from '@/store/select'
 
@@ -151,33 +125,6 @@ onMounted(() => {
     justify-content center
     align-items center
     transform-origin left top
-
-.loading
-  z-index 999
-  position absolute
-  width 100%
-  height 100%
-  background rgba(0, 0, 0, 0.5)
-  display flex
-  justify-content center
-  align-items center
-
-  .circle
-    stroke-dashoffset 0
-    animation loading 2s infinite
-    transform-origin center
-
-@keyframes loading
-  0%
-    stroke-dashoffset 0
-    transform rotate(0deg)
-
-  50%
-    stroke-dashoffset 1194
-
-  100%
-    stroke-dashoffset 0
-    transform rotate(360deg)
 
 .select
   position fixed
