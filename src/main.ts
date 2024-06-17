a18a798037f0755ff2a950742a80d4bc66a6d8da
blacktunes <blacktunes@foxmail.com>
2024-06-17 22:20:20 +0800

    feat: 增加访问统计

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { analytics } from 'star-rail-vue'
 import { createApp } from 'vue'
 import blur from './assets/scripts/blur'
 import { hotkey } from './assets/scripts/hotkey'
@@ -6,6 +7,8 @@ import JUUs from './views/JUUs/Index.vue'
 import Talk from './views/Talk/Index.vue'
 
 window.BUILD_TIME = new Date(BUILD_TIME)
+
+analytics('G-ZBY4GRVH6J', import.meta.env.MODE === 'development')
 
 createApp(import.meta.env.VITE_BUILD_MODE === 'TALK' ? Talk : JUUs)
   .use(blur)
