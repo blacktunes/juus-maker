d66d227d1e6cde31d76e5cf3845c56fad25dbb5c
blacktunes <blacktunes@foxmail.com>
2024-06-17 22:22:44 +0800

    feat: 增加数据库加载异常提示

--- src/assets/scripts/database.ts
+++ src/assets/scripts/database.ts
@@ -1,27 +1,23 @@
 import { data } from '@/store/juus'
 import { ship } from '@/store/ship'
 import { createDatabase } from 'star-rail-vue'
-// import { popupManager } from './popup'
+import { popupManager } from './popup'
 
 export const loadJUUsDatabase = () => {
   return new Promise<void>((resolve) => {
     // 数据库加载超时
-    // const timeout = setTimeout(() => {
-    //   popupManager.open('confirm', {
-    //     title: '数据库加载异常',
-    //     tip: '如果持续出现这种情况可以尝试在数据管理里重置数据库',
-    //     text: [
-    //       '加载时间过长，可能是数据损坏',
-    //       '点击<span style="color:red">确认</span>可以继续使用，但是可能出现功能异常'
-    //     ],
-    //     fn: () => {
-    //       popupManager.close('loading')
-    //     },
-    //     close: () => {
-    //       resolve()
-    //     }
-    //   })
-    // }, 30 * 1000)
+    const timeout = setTimeout(() => {
+      popupManager.open('confirm', {
+        text: [
+          '加载时间过长，可能是数据损坏',
+          '点击<span style="color:red">确定</span>可以继续使用，但是可能出现功能异常'
+        ],
+        fn: () => {},
+        close: () => {
+          resolve()
+        }
+      })
+    }, 30 * 1000)
 
     createDatabase('azur-land-juus', 'JUUs')
       .add({
@@ -36,24 +32,21 @@ export const loadJUUsDatabase = () => {
       })
       .next()
       .then(() => {
-        // popupManager.close('confirm')
-        // setDetails()
+        popupManager.close('confirm')
         resolve()
       })
       .catch((err) => {
         console.error(err)
 
-        // popupManager.open('confirm', {
-        // title: '数据库初始化失败',
-        // text: ['光锥编辑器可以正常使用', '但是数据可能丢失且不会被保存'],
-        // close: () => {
-        resolve()
-        //   }
-        // })
+        popupManager.open('confirm', {
+          text: ['数据库初始化失败', '编辑器可以正常使用', '但是数据可能丢失且不会被保存'],
+          close: () => {
+            resolve()
+          }
+        })
       })
       .finally(() => {
-        // clearTimeout(timeout)
-        // popupManager.close('loading')
+        clearTimeout(timeout)
       })
   })
 }
